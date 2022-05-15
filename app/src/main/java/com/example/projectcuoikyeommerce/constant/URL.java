0615e645b9ad319d55162e5b96212a4c7481fd7c
TrongNgo <trongtan252001@gmail.com>
2022-05-15 09:18:16 +0700

    [dev-trong] update url v2

--- app/src/main/java/com/example/projectcuoikyeommerce/constant/URL.java
+++ app/src/main/java/com/example/projectcuoikyeommerce/constant/URL.java
@@ -1,5 +1,5 @@
 package com.example.projectcuoikyeommerce.constant;
 
 public class URL {
-    public static final String BASE_URL = "http://192.168.1.3:3100/";
+    public static final String BASE_URL = "http://192.168.0.191:3100/";
 }
