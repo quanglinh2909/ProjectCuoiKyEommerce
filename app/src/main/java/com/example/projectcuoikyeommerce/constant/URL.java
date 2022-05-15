50ea8e69759c594b6b514633ca3d6b817a8b3f83
Nguyễn Văn Hậu <19130065@st.hcmuaf.edu.vn>
2022-05-15 09:47:35 +0700

    [dev-hau] commit

--- app/src/main/java/com/example/projectcuoikyeommerce/constant/URL.java
+++ app/src/main/java/com/example/projectcuoikyeommerce/constant/URL.java
@@ -2,5 +2,5 @@ package com.example.projectcuoikyeommerce.constant;
 
 public class URL {
     public static final String BASE_URL = "http://localhost:3100/";
-    public static final String BASE_URL_IPV4 = "http://192.168.1.3:3100/";
+    public static final String BASE_URL_IPV4 = "http://192.168.0.197:3100/";
 }
