fb096381c28ee09a024a8939728d4f6dfa50e8c0
quanglinh2909 <hackcoquanglinh2000@gmail.com>
2022-05-15 22:31:20 +0700

    [dev] new menu

--- app/src/main/java/com/example/projectcuoikyeommerce/component/ExpandableMenuAdapter.java
+++ app/src/main/java/com/example/projectcuoikyeommerce/component/ExpandableMenuAdapter.java
@@ -1,2 +1,66 @@
-package com.example.projectcuoikyeommerce.component;public class ExpandableMenuAdapter {
+package com.example.projectcuoikyeommerce.component;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+import java.util.List;
+import java.util.Map;
+
+public class ExpandableMenuAdapter extends BaseExpandableListAdapter  {
+    private List mListGroup;
+    private Map mListItem;
+
+    @Override
+    public int getGroupCount() {
+        if(mListGroup != null) return mListGroup.size();
+        return 0;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+//        if(mListGroup != null && mListItem != null) return  mListItem.get(mListGroup.get(groupPosition)).size();
+        return 0;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return mListGroup.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+//        return mListItem.get(mListGroup.get(groupPosition)).get(childPosition);
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
 }
--- app/src/main/java/com/example/projectcuoikyeommerce/component/MenuBottomSheet.java
+++ app/src/main/java/com/example/projectcuoikyeommerce/component/MenuBottomSheet.java
@@ -1,2 +1,29 @@
-package com.example.projectcuoikyeommerce.component;public class MenuBottomSheet {
+package com.example.projectcuoikyeommerce.component;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.projectcuoikyeommerce.R;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class MenuBottomSheet extends BottomSheetDialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        BottomSheetDialog bottomSheetDialog = (BottomSheetDialog) super.onCreateDialog(savedInstanceState);
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.menu_bottom_sheet,null);
+        bottomSheetDialog.setContentView(view);
+        bottomSheetDialog.setCancelable(false);
+
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from((View) view.getParent());
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        return bottomSheetDialog;
+    }
 }
