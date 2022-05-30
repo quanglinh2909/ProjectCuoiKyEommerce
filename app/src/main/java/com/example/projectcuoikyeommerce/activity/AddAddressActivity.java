f5afef3659767a2450a4e56b50cd687ceb7084b4
TrongNgo <trongtan252001@gmail.com>
2022-05-30 23:26:53 +0700

    [dev-trong] feat dialog component

--- app/src/main/java/com/example/projectcuoikyeommerce/activity/AddAddressActivity.java
+++ app/src/main/java/com/example/projectcuoikyeommerce/activity/AddAddressActivity.java
@@ -2,10 +2,11 @@ package com.example.projectcuoikyeommerce.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.projectcuoikyeommerce.R;
+import com.example.projectcuoikyeommerce.component.CheckoutDialog;
+import com.example.projectcuoikyeommerce.constant.Main;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.FrameLayout;
 
 public class AddAddressActivity extends AppCompatActivity {
     private Button btnCheckout;
@@ -15,6 +16,13 @@ public class AddAddressActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_address);
         initUi();
+        handleAction();
+    }
+    private void handleAction() {
+        btnCheckout.setOnClickListener(v -> {
+            CheckoutDialog cdd=new CheckoutDialog(AddAddressActivity.this);
+            cdd.show();
+        });
     }
     private void initUi() {
         btnCheckout = findViewById(R.id.btnCheckout);
---
+++ app/src/main/java/com/example/projectcuoikyeommerce/component/CheckoutDialog.java
@@ -0,0 +1,44 @@
+package com.example.projectcuoikyeommerce.component;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.projectcuoikyeommerce.R;
+
+import androidx.annotation.NonNull;
+
+public class CheckoutDialog extends Dialog implements View.OnClickListener {
+    private Context context;
+    private Button btnCheckout, btnBackToHome;
+    public CheckoutDialog(@NonNull Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.checkout_diaglog);
+        initUI();
+    }
+    public void initUI(){
+        btnCheckout = findViewById(R.id.btn_submit);
+        btnBackToHome = findViewById(R.id.btn_back);
+        btnCheckout.setOnClickListener(this);
+        btnBackToHome.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_submit:
+                break;
+            case R.id.btn_back:
+                Log.d("CheckoutDialog", "onClick: ");
+                break;
+        }
+    }
+}
